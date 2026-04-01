b28cd8977f343d312523b0201f1e2f0543f06214
krishan <astrohari09@outlook.com>
2026-04-01 10:19:20 +0530

    Update _inline.py

--- BANIYA_V3/helpers/_inline.py
+++ BANIYA_V3/helpers/_inline.py
@@ -155,7 +155,7 @@ class Inline:
                 [
                     self.ikb(
                         text=lang["source"],
-                        url="https://t.me/ArcBotz",
+                        url="https://t.me/THE_EXPOSE_NET",
                         style=enums.ButtonStyle.DANGER,
                         icon_custom_emoji_id=5983580310292402968,
                     )
