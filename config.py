e0cf7e311e99ecf82463c9221d129491f6ac9801
krishan <astrohari09@outlook.com>
2026-04-06 15:52:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_EXPOSE_NET")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EXPOSE_SUPPORT")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BDuYyghhWHhkN2Rl")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+lXaZbOeZiyBlZDg1")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "false"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
